920b5bdba439337a870409ae04ea2f3bdd53099c
Zephyr Pellerin <zv@nxvr.org>
2017-01-11 14:02:22 -0800

    Add basic cargo generated docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,66 @@
+// Copyright 2017 Zephyr "zv" Pellerin. See the COPYRIGHT
+// file at the top-level directory of this distribution
+//
+// Licensed under the MIT License, <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+
+//! S-expression parsing and serialization
+//!
+//! # What are S-expressions?
+
+
+//! # What are S-Expressions?
+//!
+//! S-expressions are data structures for representing complex data.  They
+//! are either byte-strings ("octet-strings") or lists of simpler
+//! S-expressions.  Here is a sample S-expression:
+//!
+//! Data types that can be encoded are varied, and can be expressed in multiple
+//! different ways, depending on the parser's configuration.
+//!
+//! * `Symbol`: An identifier
+//! * `I64`: equivalent to rust's `i64`
+//! * `U64`: equivalent to rust's `u64`
+//! * `F64`: equivalent to rust's `f64`
+//! * `Boolean`: equivalent to rust's `bool`
+//! * `String`: equivalent to rust's `String`
+//! * `Cons`: A structure with two pointers 'car' and 'cdr'
+//! * `Nil`: A special object, indicating an end of list or the lack of value.
+//!
+//! Composite structures are made through combination of these components
+//!
+//! # Simple Lists
+//! ```ignore
+//! (computer clock calendar)
+//! ```
+//!
+//! ## Associated Lists
+//!
+//! ```ignore
+//! (doctors  . ("Dr. Hargrove" "Dr. Steve" "Dr. Mischief" "Dr. Lizzie")
+//!  teachers . ("Ms. Taya" "Mr. Smith" "Principle Weedle")
+//!  sailors  . ("Ole Skippy" "Ravin' Dave" "Popeye"))
+//! ```
+//!
+//! # Examples of use
+//!
+//! ```rust
+//! use std::collections::BTreeMap;
+//!
+//! let values = "((New York . Albany)
+//!                (Oregon   . Salem)
+//!                (Florida  . Miami)
+//!                (California . Sacramento)
+//!                (Colorado . Denver))";
+//!
+//! let sexp = Sexp::from_str(values);
+//!
+//! // Deserialize using 'sexpattern::decode'
+//! let decoded: BTreeMap = sexpattern::decode(&sexp).unwrap();
+//!
+//! println!("Colorado's Capital is: {}", decoded.get("Colorado"))
+//! ```
 /// An s-expression is either an atom or a list of s-expressions. This is
 /// similar to the data format used by lisp.
 #[derive(PartialEq, PartialOrd, Clone, Debug)]
