73e8ef4c3f7bae0a6b2f71e2314de3b677538189
Zephyr Pellerin <zv@nxvr.org>
2017-01-11 14:05:17 -0800

    Split error handling into error.rs

---
+++ src/error.rs
@@ -0,0 +1,43 @@
+#![allow(non_snake_case)]
+
+/// The errors that can arise while parsing a S-expression stream.
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum ErrorCode {
+    InvalidSyntax,
+    InvalidAtom,
+    InvalidNumber,
+    InvalidEscape,
+    UnrecognizedBase64,
+    UnrecognizedHex,
+    UnexpectedEndOfHexEscape,
+    UnexpectedEndOfList,
+    EOFWhileParsingAtom,
+    EOFWhileParsingList,
+    EOFWhileParsingValue,
+    EOFWhileParsingNumeric,
+    EOFWhileParsingString,
+    ControlCharacterInString,
+    TrailingCharacters,
+}
+
+use self::ErrorCode::*;
+
+#[derive(Debug)]
+pub enum ParserError {
+    ///         msg,      line,   col
+    SyntaxError(ErrorCode, usize, usize),
+    // IoError(io::Error),
+}
+
+/// Returns a readable error string for a given error code.
+fn error_str(error: ErrorCode) -> &'static str {
+    match error {
+        InvalidSyntax => "invalid syntax",
+        InvalidNumber => "invalid number",
+        UnrecognizedBase64 => "Base64-encoded string can only include valid base64 characters",
+        EOFWhileParsingList => "EOF While parsing list",
+        EOFWhileParsingString => "EOF While parsing string",
+        _ => "something else entirely"
+    }
+}
