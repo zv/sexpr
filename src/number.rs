d62fe63394d8a2f35799d4057e6c06c8f1a6188c
Zephyr Pellerin <zv@nxvr.org>
2017-05-29 17:06:38 -0700

    Update Number to use Sexp-specific error codes

--- src/number.rs
+++ src/number.rs
@@ -1,6 +1,6 @@
 // Copyright 2017 Zephyr Pellerin
-
-use error::ErrorCode;
+use error::Error;
+use num_traits::NumCast;
 use serde::de::{self, Visitor, Unexpected};
 use serde::{Serialize, Serializer, Deserialize, Deserializer};
 use std::fmt::{self, Debug, Display};
@@ -12,128 +12,75 @@ pub struct Number {
     n: N,
 }
 
-// "N" is a prefix of "I64"... this is a false positive.
+// "N" is a prefix of "NegInt"... this is a false positive.
 // https://github.com/Manishearth/rust-clippy/issues/1241
 #[cfg_attr(feature = "cargo-clippy", allow(enum_variant_names))]
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum N {
-    U64(u64),
+    PosInt(u64),
     /// Always less than zero.
-    I64(i64),
+    NegInt(i64),
     /// Always finite.
-    F64(f64),
+    Float(f64),
 }
 
 impl Number {
-    /// Returns true if the `Number` is an integer between `i64::MIN` and
-    /// `i64::MAX`.
-    ///
-    /// For any Number on which `is_i64` returns true, `as_i64` is guaranteed to
-    /// return the integer value.
-    ///
-    /// ```rust
-    /// # #[macro_use]
-    /// # extern crate sexpr;
-    /// #
-    /// # use std::i64;
-    /// #
-    /// # fn main() {
-    /// let big = i64::MAX as u64 + 10;
-    /// let v = sexp!(((a 64) (b "big") (c 256.0)));
-    ///
-    /// assert!(v["a"].is_i64());
-    ///
-    /// // Greater than i64::MAX.
-    /// assert!(!v["b"].is_i64());
-    ///
-    /// // Numbers with a decimal point are not considered integers.
-    /// assert!(!v["c"].is_i64());
-    /// # }
-    /// ```
     #[inline]
     pub fn is_i64(&self) -> bool {
         match self.n {
-            N::U64(v) => v <= i64::MAX as u64,
-            N::I64(_) => true,
-            N::F64(_) => false,
+            N::PosInt(v) => v <= i64::MAX as u64,
+            N::NegInt(_) => true,
+            N::Float(_) => false,
         }
     }
 
-    /// Returns true if the `Number` is an integer between zero and `u64::MAX`.
-    ///
-    /// For any Number on which `is_u64` returns true, `as_u64` is guaranteed to
-    /// return the integer value.
-    ///
-    /// ```rust
-    /// # #[macro_use]
-    /// # extern crate sexpr;
-    /// #
-    /// # fn main() {
-    /// let v = sexp!(((a 64) (b -64) (c 256.0)));
-    ///
-    /// assert!(v["a"].is_u64());
-    ///
-    /// // Negative integer.
-    /// assert!(!v["b"].is_u64());
-    ///
-    /// // Numbers with a decimal point are not considered integers.
-    /// assert!(!v["c"].is_u64());
-    /// # }
-    /// ```
     #[inline]
     pub fn is_u64(&self) -> bool {
         match self.n {
-            N::U64(_) => true,
-            N::I64(_) | N::F64(_) => false,
+            N::PosInt(_) => true,
+            N::NegInt(_) | N::Float(_) => false,
         }
     }
 
-    /// Returns true if the `Number` can be represented by f64.
-    ///
-    /// For any Number on which `is_f64` returns true, `as_f64` is guaranteed to
-    /// return the floating point value.
-    ///
-    /// Currently this function returns true if and only if both `is_i64` and
-    /// `is_u64` return false but this is not a guarantee in the future.
-    ///
-    /// ```rust
-    /// # #[macro_use]
-    /// # extern crate sexpr;
-    /// #
-    /// # fn main() {
-    /// let v = sexp!(((a 256.0) (b 64) (c -64)));
-    ///
-    /// assert!(v["a"].is_f64());
-    ///
-    /// // Integers.
-    /// assert!(!v["b"].is_f64());
-    /// assert!(!v["c"].is_f64());
-    /// # }
-    /// ```
     #[inline]
     pub fn is_f64(&self) -> bool {
         match self.n {
-            N::F64(_) => true,
-            N::U64(_) | N::I64(_) => false,
+            N::Float(_) => true,
+            N::PosInt(_) | N::NegInt(_) => false,
         }
     }
 
-    /// Converts a finite `f64` to a `Number`. Infinite or NaN values are not SEXP
-    /// numbers.
-    ///
-    /// ```rust
-    /// # use std::f64;
-    /// #
-    /// # use sexpr::Number;
-    /// #
-    /// assert!(Number::from_f64(256.0).is_some());
-    ///
-    /// assert!(Number::from_f64(f64::NAN).is_none());
-    /// ```
+    #[inline]
+    pub fn as_i64(&self) -> Option<i64> {
+        match self.n {
+            N::PosInt(n) => NumCast::from(n),
+            N::NegInt(n) => Some(n),
+            N::Float(_) => None,
+        }
+    }
+
+    #[inline]
+    pub fn as_u64(&self) -> Option<u64> {
+        match self.n {
+            N::PosInt(n) => Some(n),
+            N::NegInt(n) => NumCast::from(n),
+            N::Float(_) => None,
+        }
+    }
+
+    #[inline]
+    pub fn as_f64(&self) -> Option<f64> {
+        match self.n {
+            N::PosInt(n) => NumCast::from(n),
+            N::NegInt(n) => NumCast::from(n),
+            N::Float(n) => Some(n),
+        }
+    }
+
     #[inline]
     pub fn from_f64(f: f64) -> Option<Number> {
         if f.is_finite() {
-            Some(Number { n: N::F64(f) })
+            Some(Number { n: N::Float(f) })
         } else {
             None
         }
@@ -143,9 +90,9 @@ impl Number {
 impl fmt::Display for Number {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         match self.n {
-            N::U64(i) => Display::fmt(&i, formatter),
-            N::I64(i) => Display::fmt(&i, formatter),
-            N::F64(f) => Display::fmt(&f, formatter),
+            N::PosInt(i) => Display::fmt(&i, formatter),
+            N::NegInt(i) => Display::fmt(&i, formatter),
+            N::Float(f) => Display::fmt(&f, formatter),
         }
     }
 }
@@ -159,13 +106,13 @@ impl Debug for Number {
 impl Serialize for Number {
     #[inline]
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-        where
+    where
         S: Serializer,
     {
         match self.n {
-            N::U64(i) => serializer.serialize_u64(i),
-            N::I64(i) => serializer.serialize_i64(i),
-            N::F64(f) => serializer.serialize_f64(f),
+            N::PosInt(i) => serializer.serialize_u64(i),
+            N::NegInt(i) => serializer.serialize_i64(i),
+            N::Float(f) => serializer.serialize_f64(f),
         }
     }
 }
@@ -173,7 +120,7 @@ impl Serialize for Number {
 impl<'de> Deserialize<'de> for Number {
     #[inline]
     fn deserialize<D>(deserializer: D) -> Result<Number, D::Error>
-        where
+    where
         D: Deserializer<'de>,
     {
         struct NumberVisitor;
@@ -197,10 +144,10 @@ impl<'de> Deserialize<'de> for Number {
 
             #[inline]
             fn visit_f64<E>(self, value: f64) -> Result<Number, E>
-                where
+            where
                 E: de::Error,
             {
-                Number::from_f64(value).ok_or_else(|| de::Error::custom("not a JSON number"))
+                Number::from_f64(value).ok_or_else(|| de::Error::custom("not a Sexp number"))
             }
         }
 
@@ -208,6 +155,49 @@ impl<'de> Deserialize<'de> for Number {
     }
 }
 
+impl<'de> Deserializer<'de> for Number {
+    type Error = Error;
+
+    #[inline]
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Error>
+    where
+        V: Visitor<'de>,
+    {
+        match self.n {
+            N::PosInt(i) => visitor.visit_u64(i),
+            N::NegInt(i) => visitor.visit_i64(i),
+            N::Float(f) => visitor.visit_f64(f),
+        }
+    }
+
+    forward_to_deserialize_any! {
+        bool i8 i16 i32 i64 u8 u16 u32 u64 f32 f64 char str string bytes
+        byte_buf option unit unit_struct newtype_struct seq tuple
+        tuple_struct map struct enum identifier ignored_any
+    }
+}
+
+impl<'de, 'a> Deserializer<'de> for &'a Number {
+    type Error = Error;
+
+    #[inline]
+    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Error>
+    where
+        V: Visitor<'de>,
+    {
+        match self.n {
+            N::PosInt(i) => visitor.visit_u64(i),
+            N::NegInt(i) => visitor.visit_i64(i),
+            N::Float(f) => visitor.visit_f64(f),
+        }
+    }
+
+    forward_to_deserialize_any! {
+        bool i8 i16 i32 i64 u8 u16 u32 u64 f32 f64 char str string bytes
+        byte_buf option unit unit_struct newtype_struct seq tuple
+        tuple_struct map struct enum identifier ignored_any
+    }
+}
 
 macro_rules! from_signed {
     ($($signed_ty:ident)*) => {
@@ -216,9 +206,9 @@ macro_rules! from_signed {
                 #[inline]
                 fn from(i: $signed_ty) -> Self {
                     if i < 0 {
-                        Number { n: N::I64(i as i64) }
+                        Number { n: N::NegInt(i as i64) }
                     } else {
-                        Number { n: N::U64(i as u64) }
+                        Number { n: N::PosInt(i as u64) }
                     }
                 }
             }
@@ -232,7 +222,7 @@ macro_rules! from_unsigned {
             impl From<$unsigned_ty> for Number {
                 #[inline]
                 fn from(u: $unsigned_ty) -> Self {
-                    Number { n: N::U64(u as u64) }
+                    Number { n: N::PosInt(u as u64) }
                 }
             }
         )*
@@ -247,9 +237,9 @@ impl Number {
     #[doc(hidden)]
     pub fn unexpected(&self) -> Unexpected {
         match self.n {
-            N::U64(u) => Unexpected::Unsigned(u),
-            N::I64(i) => Unexpected::Signed(i),
-            N::F64(f) => Unexpected::Float(f),
+            N::PosInt(u) => Unexpected::Unsigned(u),
+            N::NegInt(i) => Unexpected::Signed(i),
+            N::Float(f) => Unexpected::Float(f),
         }
     }
 }
